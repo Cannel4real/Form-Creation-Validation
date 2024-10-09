8fea472aba24b157a05b82a1c1a2f9df82b7d858
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-10-09 16:50:49 +0200

    fetching data from an API

---
+++ fetch-data.js
@@ -0,0 +1,32 @@
+async function fetchUserData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users'
+    
+    const dataContainer = document.getElementById('api-data');
+
+    try {   
+      const response = await fetch (apiUrl);
+      const users = await response.json();
+      dataContainer.innerHTML = '';
+
+      userList = document.createElement('ul');
+        users.forEach(user => {
+        let listItemContent = document.createElement('li');
+        listItemContent.textContent = `${user.name} `;
+    });
+    
+    dataContainer.appendChild(userList);
+    console.log(userList);
+        
+    } catch (error) {
+        dataContainer.textContent ='Failed to load user data.'
+    }
+    
+}
+
+// Call the function to fetch user data when the page loads
+
+window.addEventListener('DOMContentLoaded', () => {
+    fetchUserData();
+});
+
+fetchUserData();
